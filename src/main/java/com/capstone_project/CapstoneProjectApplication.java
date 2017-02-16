eb13001a7a814e5724ffdf57997caea57958c297
nedwaldie <nw_waldie3@yahoo.com>
2017-02-16 11:24:41 -0600

    Spring boot initializer

---
+++ src/main/java/com/capstone_project/CapstoneProjectApplication.java
@@ -0,0 +1,12 @@
+package com.capstone_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneProjectApplication.class, args);
+	}
+}

f9d559e98140d3aaaeadd3148c7941e730950087
hmt3design <hmt3design@gmail.com>
2017-02-17 15:24:19 -0600

    021717 - incorporated Style Bootstrap theme

--- src/main/resources/static/javaScript/custom.js
+++ src/main/resources/static/javaScript/custom.js
@@ -2,7 +2,7 @@
  * Created by jefferysmith on 2/16/17.
  */
 // Closes the sidebar menu
-$("#menu-close").click(function(e) {
+$('#menu-close').click(function(e) {
     e.preventDefault();
     $("#sidebar-wrapper").toggleClass("active");
 });

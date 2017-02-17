573e53dffda4696423ec646902b884b107995485
nedwaldie <nw_waldie3@yahoo.com>
2017-02-17 15:33:48 -0600

    Created models for merchants and users

--- src/main/java/com/capstone_project/models/Users.java
+++ src/main/java/com/capstone_project/models/Users.java
@@ -4,4 +4,67 @@ package com.capstone_project.models;
  * Created by nedwaldie on 2/16/17.
  */
 public class Users {
+    private int id;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String company;
+    private String phone;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
--- src/main/java/com/capstone_project/models/UsersRole.java
+++
@@ -1,7 +0,0 @@
-package com.capstone_project.models;
-
-/**
- * Created by nedwaldie on 2/16/17.
- */
-public class UsersRole {
-}

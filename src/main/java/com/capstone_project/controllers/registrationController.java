a4d7406b5ab6814308c0a3cf3f3f6d7d0527e8eb
nedwaldie <nw_waldie3@yahoo.com>
2017-02-20 16:48:33 -0600

    Working on creating the post mapping for the registration form.

--- src/main/java/com/capstone_project/controllers/registrationController.java
+++ src/main/java/com/capstone_project/controllers/registrationController.java
@@ -1,16 +1,26 @@
 package com.capstone_project.controllers;
 
 import com.capstone_project.models.User;
+import com.capstone_project.repositories.Users;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
 
 /**
  * Created by nedwaldie on 2/20/17.
  */
 @Controller
 public class registrationController {
+    @Autowired
+    Users userRepository;
+
+
     @GetMapping("/register")
     public String registrationPage(Model model) {
         model.addAttribute("user", new User());
@@ -18,8 +28,21 @@ public class registrationController {
     }
 
     @PostMapping("/register")
-    public String register() {
-        return "";
-    }
+    public String registerUser(@Valid User user, Errors validation, Model model, @RequestParam(name = "password_confirm") String passwordconfirmation) {
+//        if (!passwordconfirmation.equals(user.getPassword())) {
+//            validation.rejectValue("password", "user.password", "Your passwords do not match");
+//        }
+//        if (validation.hasErrors()) {
+//            model.addAttribute("errors", validation);
+//            model.addAttribute("user", user);
+//            return "/posts/register";
+//        }
 
+
+        String hashedPassword = encoder.encode(user.getPassword());
+        user.setPassword(hashedPassword);
+        User newUser = userRepository.save(user);
+
+        return "redirect:/login";
+    }
 }

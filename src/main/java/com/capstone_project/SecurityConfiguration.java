bc9a38842900173841083c1affbc86565c34c2d9
nedwaldie <nw_waldie3@yahoo.com>
2017-02-23 16:39:15 -0600

    working on login page post mapping.

--- src/main/java/com/capstone_project/SecurityConfiguration.java
+++ src/main/java/com/capstone_project/SecurityConfiguration.java
@@ -31,7 +31,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http
                 .formLogin()
                 .loginPage("/login")
-                .defaultSuccessUrl("/me") // user's home page, it can be any URL
+                .defaultSuccessUrl("/home") // user's home page, it can be any URL
                 .permitAll() // Anyone can go to the login page
                 .and()
                 .authorizeRequests()

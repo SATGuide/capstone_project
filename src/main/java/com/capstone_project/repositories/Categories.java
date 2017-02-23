29d6f4fe2a11e4d4d9c81784ef9636ddd16f3d76
nedwaldie <nw_waldie3@yahoo.com>
2017-02-23 12:10:38 -0600

    Connected the business control to the database.

---
+++ src/main/java/com/capstone_project/repositories/Categories.java
@@ -0,0 +1,19 @@
+package com.capstone_project.repositories;
+
+import com.capstone_project.models.Category;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Created by nedwaldie on 2/23/17.
+ */
+@Repository
+public interface Categories extends CrudRepository<Category, Integer>{
+    @Query("SELECT c FROM Category c where c.category like ?1")
+    public List<Category> findByCategory(String category);
+
+
+}

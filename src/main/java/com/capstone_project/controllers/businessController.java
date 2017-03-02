5a345b71272f0f6f66855c964a8fdc5a51528032
nedwaldie <nw_waldie3@yahoo.com>
2017-03-02 11:32:55 -0600

    editing the profile page to show what the users have added to the page.

--- src/main/java/com/capstone_project/controllers/businessController.java
+++ src/main/java/com/capstone_project/controllers/businessController.java
@@ -2,9 +2,11 @@ package com.capstone_project.controllers;
 
 import com.capstone_project.models.Category;
 import com.capstone_project.models.Merchant;
+import com.capstone_project.models.User;
 import com.capstone_project.repositories.Categories;
 import com.capstone_project.repositories.Merchants;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -36,7 +38,9 @@ public class businessController {
 
     @PostMapping("/merchant/add")
     public String addNewMerchant(@Valid Merchant merchant) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        merchant.setUser(user);
         merchantRepo.save(merchant);
-        return "redirect:/home";
+        return "redirect:/profile";
     }
 }

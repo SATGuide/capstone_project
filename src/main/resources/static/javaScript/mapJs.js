97a4d2ab52c12f96aad1ea1b6d9758bec4445f49
jefferysmith <buckeyecoder@gmail.com>
2017-03-06 13:36:20 -0600

    Added file to support geo location for new businesses

--- src/main/resources/static/javaScript/mapJs.js
+++ src/main/resources/static/javaScript/mapJs.js
@@ -227,6 +227,10 @@
         setMapOnAll(markers)
     }
 
+
+
+
+
     function pinSymbol(color) {
         return {
             path: 'M 0,0 C -2,-20 -10,-22 -10,-30 A 10,10 0 1,1 10,-30 C 10,-22 2,-20 0,0 z',
@@ -259,7 +263,5 @@
         clearMarkers();
         markers = [];
     }
-
-
 
 })();
